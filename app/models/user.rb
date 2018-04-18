1d2878edbc265d697259c86d8cf7789d25a3cc4f
Yao <GyonGyon@users.noreply.github.com>
2018-04-18 10:05:22 +0800

    update session and login and remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
 
-  log('user<applicationRecord')
   # 存入数据前将 此属性值转化为小写
   # before_save { email.downcase! } 等价
   before_save { self.email = email.downcase }
@@ -22,5 +22,18 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
