1fe2c3955d643a95bbe289af1059279800d778bd
Yao <GyonGyon@users.noreply.github.com>
2018-04-18 15:11:33 +0800

    update user admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417074829) do
+ActiveRecord::Schema.define(version: 20180418063737) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180417074829) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
